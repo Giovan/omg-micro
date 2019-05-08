3bea1bf0e896dc0ccafe76d8a74047188bd05dae
Kenneth Reitz <me@kennethreitz.org>
2019-05-08 18:50:10 -0400

    micro

--- micro.py
+++ micro.py
@@ -2,9 +2,23 @@ import delegator
 import yaml
 
 
+YML_TEMPLATE = """
+omg: 1
+""".strip()
+
+
 class Microservice:
     def __init__(self):
         pass
+
+    def _generate_yaml(self):
+        data = yaml.safe_loads(YML_TEMPLATE)
+        return data
+
+    @staticmethod
+    def _format_args(**args):
+        args = [f'{k}={v}' for (k, v) in args.items()]
+        return ' '.join(args)
 
     def ensure_yml(self, overwrite=True):
         pass
@@ -12,9 +26,11 @@ class Microservice:
     def build(self):
         self.ensure_yml()
         c = delegator.run("omg build")
+        return c.ok
 
     def run(self, command, **args):
         self.ensure_yml()
+        args = self._format_args(**args)
         pass
 
 
